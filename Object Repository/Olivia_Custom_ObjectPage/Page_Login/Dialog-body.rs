a557854eb48b5656d72f2d11516c24a65bd6ed52
Thanhduong8234 <Thanh.tat.duong8234@gmail.com>
2022-05-25 12:10:57 +0700

    commit

---
+++ Object Repository/Olivia_Custom_ObjectPage/Page_Login/Dialog-body.rs
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<WebElementEntity>
+   <description></description>
+   <name>Dialog-body</name>
+   <tag></tag>
+   <elementGuidId>a5450c4f-8930-4f30-84f2-2b507bd2f1a6</elementGuidId>
+   <selectorCollection>
+      <entry>
+         <key>BASIC</key>
+         <value></value>
+      </entry>
+      <entry>
+         <key>XPATH</key>
+         <value>//div[@class=&quot;el-dialog__body&quot;]</value>
+      </entry>
+   </selectorCollection>
+   <selectorMethod>XPATH</selectorMethod>
+   <useRalativeImagePath>false</useRalativeImagePath>
+</WebElementEntity>
